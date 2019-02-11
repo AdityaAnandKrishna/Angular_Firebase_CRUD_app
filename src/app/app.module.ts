35cb1cd0c9fd02b6c2638638291acf8d10de02c4
Aditya Anand <adityaananad@gmail.com>
2019-02-11 21:00:19 +0530

    angular crud app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 import { AppComponent } from './app.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { EmployeeComponent } from './employees/employee/employee.component';
+import { EmployeeListComponent } from './employees/employee-list/employee-list.component';
+import { environment } from '../environments/environment';
+
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, 
+    EmployeesComponent,
+    EmployeeComponent,
+    EmployeeListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ToastrModule,
+    ToastrModule.forRoot(),
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {    
+    apiKey: "AIzaSyC6TCbmMUcaOQDOQceNODlDVlVyNX5nQJg",
+    authDomain: "angular5crud-14e77.firebaseapp.com",
+    databaseURL: "https://angular5crud-14e77.firebaseio.com",
+    projectId: "angular5crud-14e77",
+    storageBucket: "angular5crud-14e77.appspot.com",
+    messagingSenderId: "291976060439"
+  }
 };
 
 /*
